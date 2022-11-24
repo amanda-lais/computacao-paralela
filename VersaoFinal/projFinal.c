5ef6506cf37fcd685aeb7baedf68616e34306f1a
NicolasAltt <101070201+NicolasAltt@users.noreply.github.com>
2022-11-24 04:52:05 -0300

    Update projFinal.c

--- VersaoFinal/projFinal.c
+++ VersaoFinal/projFinal.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <time.h>    // for clock_t, clock()
 
-#define BILLION  1000000000.0
+long double E1 = 0.0;
+long double E2 = 0.0;
 
 void * Taylor (void *arg)
 {
   int *var = (int *)(arg);
-  int T = 100000;
+  int T = 1000000;
   int i,j;
   double f = 1.0;
-  double E = 0.0;
   
   if (*var == 1)
   {
@@ -24,7 +23,7 @@ void * Taylor (void *arg)
       {
         f = f*j;
       }
-      E += 1.0/f;
+      E1 += 1.0/f;
     }
   }
   if (*var == 2)
@@ -39,29 +38,29 @@ void * Taylor (void *arg)
       {
         f = f*j;
       }
-      E += 1.0/f;
+      E2 += 1.0/f;
     }
   }
-  printf(" E= %.100f", E);
+  //printf(" E= %.100f", E);
 }
 
 
 int main(void) 
 {
-  struct timespec start, end;
-  clock_gettime(CLOCK_REALTIME, &start);
   //Taylor();
   int i = 0;
   pthread_t t1,t2;
   int a1 =1;
   int a2 =2;
+  
+  //long double **somaFinal;
   pthread_create(&t1, NULL, Taylor, (void *)(&a1));
   pthread_create(&t2, NULL, Taylor, (void *)(&a2));
+  
   pthread_join(t1, NULL);
   pthread_join(t2, NULL);
-  //exit (0);
-  clock_gettime(CLOCK_REALTIME, &end);
-  double time_spent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION;
-  printf("The elapsed time is %f seconds", time_spent);
+  
+  printf(" E= %.60Lf", (E1+E2));
+  
   return 0;
 }
